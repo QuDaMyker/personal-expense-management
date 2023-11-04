78d8078874404ddf87f40f4338c12dadf80d8d19
VN The Boss Baby <93357243+QuDaMyker@users.noreply.github.com>
2023-11-04 08:46:57 +0700

    add Login

---
+++ app/src/main/java/com/learning/personal_expense_management/controller/activity/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.learning.personal_expense_management.controller.activity.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.learning.personal_expense_management.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
