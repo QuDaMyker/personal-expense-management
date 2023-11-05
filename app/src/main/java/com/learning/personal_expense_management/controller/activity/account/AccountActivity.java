18f5f31c48e14f700011bf077fb62d11c3421bc5
nhubaole <okamisameko7123@gmail.com>
2023-11-05 11:21:46 +0700

    account ui

---
+++ app/src/main/java/com/learning/personal_expense_management/controller/activity/account/AccountActivity.java
@@ -0,0 +1,16 @@
+package com.learning.personal_expense_management.controller.activity.account;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.learning.personal_expense_management.R;
+
+public class AccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+    }
+}
