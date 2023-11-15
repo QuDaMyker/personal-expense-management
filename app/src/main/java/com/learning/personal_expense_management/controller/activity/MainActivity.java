f1297d559755863ea1c0f568f4b43bc4c7d80619
Nhongnhong-0101 <21522423@gm.uit.edu.vn>
2023-11-15 23:14:01 +0700

    new category screen

--- app/src/main/java/com/learning/personal_expense_management/controller/activity/MainActivity.java
+++ app/src/main/java/com/learning/personal_expense_management/controller/activity/MainActivity.java
@@ -1,16 +1,28 @@
 package com.learning.personal_expense_management.controller.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 import com.learning.personal_expense_management.R;
+import com.learning.personal_expense_management.controller.fragment.NewCategoryFragment;
+import com.learning.personal_expense_management.controller.fragment.StatisticFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    FragmentManager fragmentManager = getSupportFragmentManager();
+    FragmentTransaction transaction = fragmentManager.beginTransaction();
+    NewCategoryFragment fragment = new NewCategoryFragment();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        transaction.add(R.id.fragment_container, fragment);
+        transaction.commit();
+
     }
+
+
 }
