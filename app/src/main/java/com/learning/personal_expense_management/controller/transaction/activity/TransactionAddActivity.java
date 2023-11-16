7a492d0e20ece50816be9acd4685184a8ca3a1f1
VN The Boss Baby <93357243+QuDaMyker@users.noreply.github.com>
2023-11-16 12:59:19 +0700

    add iu some logic

--- app/src/main/java/com/learning/personal_expense_management/controller/transaction/activity/TransactionAddActivity.java
+++ app/src/main/java/com/learning/personal_expense_management/controller/transaction/activity/TransactionAddActivity.java
@@ -8,15 +8,31 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.icu.util.Calendar;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
+import com.google.type.DateTime;
 import com.learning.personal_expense_management.R;
 import com.learning.personal_expense_management.databinding.ActivityTransactionAddBinding;
 import com.learning.personal_expense_management.databinding.ActivityTransactionFilterBinding;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Year;
+
 public class TransactionAddActivity extends AppCompatActivity {
     private ActivityTransactionAddBinding binding;
+    private ActivityResultLauncher<Intent> addWalletLaunch;
+    private ActivityResultLauncher<Intent> addAccountLaunch;
+    private ActivityResultLauncher<Intent> addCategoryLaunch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,17 +40,85 @@ public class TransactionAddActivity extends AppCompatActivity {
         binding = ActivityTransactionAddBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+
         init();
         setListeners();
     }
 
     private void init() {
+        addWalletLaunch = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
 
+                    }
+                }
+        );
+
+        addAccountLaunch = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+
+                    }
+                }
+        );
     }
 
     private void setListeners() {
-        binding.btnBack.setOnClickListener(v-> {
+        binding.btnBack.setOnClickListener(v -> {
             finish();
         });
+
+        binding.clVitien.setOnClickListener(v-> {
+            Intent intentAddWallet = new Intent(TransactionAddActivity.this, TransactionAddActivity_SelectionWallet.class);
+            addWalletLaunch.launch(intentAddWallet);
+        });
+
+        binding.clTaikhoan.setOnClickListener(v-> {
+            Intent intentAddAccount = new Intent(TransactionAddActivity.this, TransactionAddActivity_SelectionAccount.class);
+            addAccountLaunch.launch(intentAddAccount);
+        });
+
+
+        binding.clDanhmuc.setOnClickListener(v-> {
+            //Intent intentAddCategory = new Intent(TransactionAddActivity.this, )
+        });
+
+        binding.btnTransactionDay.setOnClickListener(v -> {
+            openDatePickerDialog();
+        });
+
+        binding.btnTransactionTime.setOnClickListener(v-> {
+            openTimePickerDialog();
+        });
+
+    }
+
+    private void openDatePickerDialog() {
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        int month = Calendar.getInstance().get(Calendar.MONTH);
+        int day = Calendar.getInstance().get(Calendar.DATE);
+        DatePickerDialog datePickerDialog = new DatePickerDialog(TransactionAddActivity.this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                binding.editTransactionDay.setText(dayOfMonth + " - " + month + " - " + year);
+            }
+        }, year, month, day);
+
+        datePickerDialog.show();
+    }
+
+    private void openTimePickerDialog() {
+        TimePickerDialog timePickerDialog = new TimePickerDialog(TransactionAddActivity.this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                binding.editTransactionTime.setText(minute + " : " + hourOfDay);
+            }
+        }, 00, 00, true);
+
+        timePickerDialog.show();
     }
 }
--- app/src/main/java/com/learning/personal_expense_management/controller/transaction/activity/TransactionAddActivity_SelectionAccount.java
+++ app/src/main/java/com/learning/personal_expense_management/controller/transaction/activity/TransactionAddActivity_SelectionAccount.java
@@ -5,12 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.learning.personal_expense_management.R;
+import com.learning.personal_expense_management.databinding.ActivityTransactionAddSelectionAccountBinding;
 
 public class TransactionAddActivity_SelectionAccount extends AppCompatActivity {
+    private ActivityTransactionAddSelectionAccountBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_transaction_add_selection_account);
+        binding = ActivityTransactionAddSelectionAccountBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        init();
+        setListeners();
     }
+
+    private void init() {
+
+    }
+
+    private void setListeners() {
+        binding.btnBack.setOnClickListener(v-> {
+            finish();
+        });
+    }
+
 }
